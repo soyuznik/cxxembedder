7c7bfa65561fbe94f9d5cdcd3e7db2ce2f6d27f5
Vasika-uso <83570527+Vasika-uso@users.noreply.github.com>
2022-03-06 20:43:04 +0200

    succesfully implemented multiple functions

--- MAIN.cpp
+++ MAIN.cpp
@@ -9,8 +9,101 @@
 using namespace std;
 #define PYSRCDIR "/PythonSrc/"
 
+PyObject* pName, * pModule, * pFunc;
+PyObject* pArgs, * pValue;
+int i;
+string file;
 
+namespace Keys {
+   char     ADD = '\ue025'         ;
+   char     ALT = '\ue00a'         ;
+   char     ARROW_DOWN = '\ue015'  ;
+   char     ARROW_LEFT = '\ue012'  ;
+   char     ARROW_RIGHT = '\ue014' ;
+   char     ARROW_UP = '\ue013'    ;
+   char     BACKSPACE = '\ue003'   ;
+   char     BACK_SPACE = '\ue003'  ;
+   char     CANCEL = '\ue001'      ;
+   char     CLEAR = '\ue005'       ;
+   char     COMMAND = '\ue03d'     ;
+   char     CONTROL = '\ue009'     ;
+   char     DECIMAL = '\ue028'     ;
+   char     DELETE = '\ue017'      ;
+   char     DIVIDE = '\ue029'      ;
+   char     DOWN = '\ue015'        ;
+   char     END = '\ue010'         ;
+   char     ENTER = '\ue007'       ;
+   char     EQUALS = '\ue019'      ;
+   char     ESCAPE = '\ue00c'      ;
+   char     F1 = '\ue031'          ;
+   char     F10 = '\ue03a'         ;
+   char     F11 = '\ue03b'         ;
+   char     F12 = '\ue03c'         ;
+   char     F2 = '\ue032'          ;
+   char     F3 = '\ue033'          ;
+   char     F4 = '\ue034'          ;
+   char     F5 = '\ue035'          ;
+   char     F6 = '\ue036'          ;
+   char     F7 = '\ue037'          ;
+   char     F8 = '\ue038'          ;
+   char     F9 = '\ue039'          ;
+   char     HELP = '\ue002'        ;
+   char     HOME = '\ue011'        ;
+   char     INSERT = '\ue016'      ;
+   char     LEFT = '\ue012'        ;
+   char     LEFT_ALT = '\ue00a'    ;
+   char     LEFT_CONTROL = '\ue009';
+   char     LEFT_SHIFT = '\ue008'  ;
+   char     META = '\ue03d'        ;
+   char     MULTIPLY = '\ue024'    ;
+   char     NUMPAD0 = '\ue01a'     ;
+   char     NUMPAD1 = '\ue01b'     ;
+   char     NUMPAD2 = '\ue01c'     ;
+   char     NUMPAD3 = '\ue01d'     ;
+   char     NUMPAD4 = '\ue01e'     ;
+   char     NUMPAD5 = '\ue01f'     ;
+   char     NUMPAD6 = '\ue020'     ;
+   char     NUMPAD7 = '\ue021'     ;
+   char     NUMPAD8 = '\ue022'     ;
+   char     NUMPAD9 = '\ue023'     ;
+   char     PAGE_DOWN = '\ue00f'   ;
+   char     PAGE_UP = '\ue00e'     ;
+   char     PAUSE = '\ue00b'       ;
+   char     RETURN = '\ue006'      ;
+   char     RIGHT = '\ue014'       ;
+   char     SEMICOLON = '\ue018'   ;
+   char     SEPARATOR = '\ue026'   ;
+   char     SHIFT = '\ue008'       ;
+   char     SPACE = '\ue00d'       ;
+   char     SUBTRACT = '\ue027'    ;
+   char     TAB = '\ue004'         ;
+   char     UP = '\ue013'          ;
+}
 
+inline int  __initialize_path(string __file) {
+    file = __file;
+    /* This is a way to get the current directory of the notebook. */
+    std::filesystem::path path = std::filesystem::current_path();
+    std::string _path = path.string();
+    /* A way to get the current directory of the notebook. */
+    std::replace(_path.begin(), _path.end(), '\\', '/');
+    try {
+        PyRun_SimpleString("import sys");
+    }
+    catch (...) {
+        cout << "cpp >> INITIALIZE with <Py_Initialize();> first\n";
+        return -1;
+    }
+    PyRun_SimpleString((string("sys.path.append(\"") + _path + string(PYSRCDIR) + string("\")")).c_str());
+    
+    pName = PyUnicode_DecodeFSDefault(file.c_str());
+    /* Error checking of pName left out */
+
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    return 0;
+}
 /**
  * It calls a python function from C++
  * 
@@ -19,24 +112,8 @@ using namespace std;
  * 
  * @return The return value is the status.
  */
- int __callpy_noarg(string file, string funcptr) {
-    PyObject* pName, * pModule, * pFunc;
-    PyObject* pArgs, * pValue;
-    int i;
+inline int __callpy_noarg(string funcptr) {
    
-    /* This is a way to get the current directory of the notebook. */
-    std::filesystem::path path = std::filesystem::current_path();
-    std::string _path = path.string();
-    /* A way to get the current directory of the notebook. */
-    std::replace(_path.begin(), _path.end(), '\\', '/');
-    Py_Initialize();
-    PyRun_SimpleString("import sys");
-    PyRun_SimpleString((string("sys.path.append(\"") + _path + string(PYSRCDIR) + string("\")")).c_str());
-    pName = PyUnicode_DecodeFSDefault(file.c_str());
-    /* Error checking of pName left out */
-    pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
-
     if (pModule != NULL) {
         pFunc = PyObject_GetAttrString(pModule, funcptr.c_str());
         /* pFunc is a new reference */
@@ -58,13 +135,9 @@ using namespace std;
         fprintf(stderr, "Failed to load \"%s\"\n", file.c_str());
         return 1;
     }
-    if (Py_FinalizeEx() < 0) {
-        return 120;
-
-    }
+    
     return 0;
 }
-  
  /**
   * It calls a Python function with args from C++.
   * 
@@ -74,25 +147,8 @@ using namespace std;
   * 
   * @return The return value is the status.
   */
- int __callpy_warg(string file, string funcptr, vector<string>* args) {
-     PyObject* pName, * pModule, * pFunc;
-     PyObject* pArgs, * pValue;
-     int i;
-     /* This is a way to get the current directory of the notebook. */
-     std::filesystem::path path = std::filesystem::current_path();
-     std::string _path = path.string();
-     /* A way to get the current directory of the notebook. */
-     std::replace(_path.begin(), _path.end(), '\\', '/');
-     Py_Initialize();
-     PyRun_SimpleString("import sys");
-     PyRun_SimpleString((string("sys.path.append(\"") + _path + string(PYSRCDIR) + string("\")")).c_str());
-    
-     pName = PyUnicode_DecodeFSDefault(file.c_str());
-     /* Error checking of pName left out */
-
-     pModule = PyImport_Import(pName);
-     Py_DECREF(pName);
-
+inline int __callpy_warg(string funcptr, vector<string>* args) {
+     
      if (pModule != NULL) {
          pFunc = PyObject_GetAttrString(pModule, funcptr.c_str());
          /* pFunc is a new reference */
@@ -138,9 +194,7 @@ using namespace std;
          fprintf(stderr, "Failed to load \"%s\"\n", file.c_str());
          return 1;
      }
-     if (Py_FinalizeEx() < 0) {
-         return 120;
-     }
+     
      return 0;
  }
 
@@ -149,10 +203,33 @@ using namespace std;
  */
 int main()
 {
-    vector<string> args;
-    args.push_back("5");
-    args.push_back("8");
-    int result = __callpy_warg("pyemb2", "test", &args);
-    cout << "__call exited with code --   " + std::to_string(result) << endl;
+    Py_Initialize();// ENTRY PYTHON
+
+
+    __initialize_path("pyemb3");
+    std::vector<string>* vec1 = new std::vector<string>;
+    vec1->push_back(" ");
+    __callpy_warg("init_selenium", vec1);
+    delete vec1;
+
+    std::vector<string>* vec2 = new std::vector<string>;
+    vec2->push_back("https://www.google.com/");
+    __callpy_warg("get_search", vec2);
+    delete vec2;
+
+    ///html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div[1]/div/div[2]/input
+
+    std::vector<string>* vec3 = new std::vector<string>;
+    vec3->push_back("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div[1]/div/div[2]/input");
+    __callpy_warg("get_elem_XPATH", vec3);
+    delete vec3;
+
+    std::vector<string>* vec4 = new std::vector<string>;
+    vec4->push_back("0");
+    __callpy_warg("click_ELEMENT", vec4);
+    delete vec4;
+
+    Py_Finalize(); // EXIT PYTHON
+    
    
 }
--- PythonSrc/pyemb3.py
+++ PythonSrc/pyemb3.py
@@ -1,5 +1,5 @@
-
 # It imports the necessary modules to run the code.
+
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,23 +7,134 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import threading
-
 import chromedriver_autoinstaller
+
 chromedriver_autoinstaller.install()
 
-def exec_thread():
-    """
-    It opens a browser window and searches google.com
-    """
+global option
+driver = ""
+elements_list = []
+
+key_list = [
+{ 'NULL' :  u'\ue000'},
+{ 'CANCEL:' : u'\ue001'  },
+{ 'HELP' :  u'\ue002'},
+{ 'BACKSPACE' : u'\ue003'},
+{ 'TAB' : u'\ue004'},
+{ 'CLEAR' : u'\ue005'},
+{ 'RETURN' : u'\ue006'},
+{ 'ENTER' : u'\ue007'},
+{ 'SHIFT' : u'\ue008'},
+{ 'CONTROL' : u'\ue009'},
+{ 'ALT' : u'\ue00a'},
+{ 'PAUSE' : u'\ue00b'},
+{ 'ESCAPE' : u'\ue00c'},
+{ 'SPACE' : u'\ue00d'},
+{ 'PAGE_UP' : u'\ue00e'},
+{ 'PAGE_DOWN' : u'\ue00f'},
+{ 'END' : u'\ue010'},
+{ 'HOME' : u'\ue011'},
+{ 'LEFT' : u'\ue012'},
+{ 'UP' : u'\ue013'},
+{ 'RIGHT' : u'\ue014'},
+{ 'DOWN' : u'\ue015'},
+{ 'INSERT' : u'\ue016'},
+{ 'DELETE' : u'\ue017'},
+{ 'SEMICOLON' : u'\ue018'},
+{ 'EQUALS' : u'\ue019'},
+{ 'NUMPAD0' : u'\ue01a'  },
+{ 'NUMPAD1' : u'\ue01b'},
+{ 'NUMPAD2' : u'\ue01c'},
+{ 'NUMPAD3' : u'\ue01d'},
+{ 'NUMPAD4' : u'\ue01e'},
+{ 'NUMPAD5' : u'\ue01f'},
+{ 'NUMPAD6' : u'\ue020'},
+{ 'NUMPAD7' : u'\ue021'},
+{ 'NUMPAD8' : u'\ue022'},
+{ 'NUMPAD9' : u'\ue023'},
+{ 'MULTIPLY' : u'\ue024'},
+{ 'ADD' : u'\ue025'},
+{ 'SEPARATOR' : u'\ue026'},
+{ 'SUBTRACT' : u'\ue027'},
+{ 'DECIMAL' : u'\ue028'},
+{ 'DIVIDE' : u'\ue029'},
+{ 'F1' : u'\ue031'  },
+{ 'F2' : u'\ue032'},
+{ 'F3' : u'\ue033'},
+{ 'F4' : u'\ue034'},
+{ 'F5' : u'\ue035'},
+{ 'F6' : u'\ue036'},
+{ 'F7' : u'\ue037'},
+{ 'F8' : u'\ue038'},
+{ 'F9' : u'\ue039'},
+{ 'F10' : u'\ue03a'},
+{ 'F11' : u'\ue03b'},
+{ 'F12' : u'\ue03c'},
+{ 'META' : u'\ue03d'},
+{ 'COMMAND' : u'\ue03d'}
+]
+
+
+def init_selenium(param):
     options = Options()
-    #options.add_argument("--headless")
-    
+    if not param == "":
+        paramlist = param.split(" ");
+    else:
+        print("init_selenium --NO ARGS")
+    if "headless" in paramlist:
+        options.add_argument("--headless")
+        print("init_selenium --HEADLESS")
+
+    global driver;
     driver = webdriver.Chrome(options=options)
 
-    searchpath = "https://google.com"
-    driver.get(searchpath);
-   
-    time.sleep(9)
 
+def get_search(PATH):
+    global driver;
+    if not PATH == "":
+        driver.get(PATH)
+    else:
+        print("get_search --NO PATH SPECIFIED")
+
+
+def wait_elem_XPATH(ELEMDATA , TIME):
+    global driver;
+    try:
+        element1 = WebDriverWait(driver, int(TIME)).until(
+            EC.presence_of_element_located((By.XPATH, ELEMDATA)))
+    except:
+        print("wait_elem_XPATH --INVALID ARGS")
+
+def get_elem_XPATH(ELEMDATA):
+    global driver;
+    element3 = driver.find_element_by_xpath(ELEMDATA)
+    global elements_list;
+    elements_list.append(element3);
+
+
+def get_elem_NAME(ELEMDATA):
+    global driver;
+    element3 = driver.find_element_by_name(ELEMDATA)
+    global elements_list;
+    elements_list.append(element3);
+
+def click_ELEMENT(ELEMNUMBER):
+    elements_list[int(ELEMNUMBER)].click()
+
+def type_ELEMENT(ELEMNUMBER , TEXT):
+    elements_list[int(ELEMNUMBER)].send_keys(TEXT)
+
+def driver_QUIT():
+    global driver;
     driver.quit()
 
+
+def driver_WAIT(sec):
+    driver.implicitly_wait(int(sec)) # seconds
+
+init_selenium(" ")
+get_search("https://www.google.com/")
+get_elem_NAME("q")
+click_ELEMENT("0")
+type_ELEMENT("0" , "HELLO")
+type_ELEMENT("0" , u'\ue007')
