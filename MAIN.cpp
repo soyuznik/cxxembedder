cc4b195259ad34a470c09117c9e2229ca30174a2
Vasika-uso <83570527+Vasika-uso@users.noreply.github.com>
2022-03-06 13:31:41 +0200

    added vscode support & gitignore

--- MAIN.cpp
+++ MAIN.cpp
@@ -19,13 +19,15 @@ using namespace std;
  * 
  * @return The return value is the return value of the function that is being called.
  */
-int __callpy_noarg(string file, string funcptr) {
+ int __callpy_noarg(string file, string funcptr) {
     PyObject* pName, * pModule, * pFunc;
     PyObject* pArgs, * pValue;
     int i;
    
+    /* This is a way to get the current directory of the notebook. */
     std::filesystem::path path = std::filesystem::current_path();
     std::string _path = path.string();
+    /* A way to get the current directory of the notebook. */
     std::replace(_path.begin(), _path.end(), '\\', '/');
     Py_Initialize();
     PyRun_SimpleString("import sys");
@@ -43,6 +45,7 @@ int __callpy_noarg(string file, string funcptr) {
             pValue = PyObject_CallObject(pFunc , NULL);
         }
         else {
+            /* Checking if there is an error. If there is an error, it prints it out. */
             if (PyErr_Occurred())
                 PyErr_Print();
             fprintf(stderr, "Cannot find function \"%s\"\n", funcptr);
@@ -62,12 +65,13 @@ int __callpy_noarg(string file, string funcptr) {
     return 0;
 }
     
+
 /**
  * It calls the Python function `getInteger` from the `pyemb3` module.
  */
 int main()
 {
     int result = __callpy_noarg("pyemb3", "getInteger");
-    cout << "__call exited with code --   " + std::to_string(result);
+    cout << "__call exited with code --   " + std::to_string(result) << endl;
    
 }
