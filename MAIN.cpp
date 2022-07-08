91a6d6e736fd495065418ca9825e019dde57825c
Vasika-uso <iovunicu@mail.ru>
2022-07-08 22:36:35 +0300

    added stringizing macro

--- MAIN.cpp
+++ MAIN.cpp
@@ -3,24 +3,25 @@
 #define PY_SSIZE_T_CLEAN
 #include <python/pyhelper.hpp>
 
-
-
+#define START_PYTHON CPyInstance inst; __initialize_path("/PythonSrc/");
+#define RUN_PYTHON(x) PyRun_SimpleString(#x);
 int main()
 {
-    CPyInstance inst;
-    __initialize_path("/PythonSrc/");
-    PyRun_SimpleString(
-        "from selenium.webdriver.chrome.options import Options\n"
-        "from selenium import webdriver\n"
-        "from selenium.webdriver.common.by import By\n"
-        "from selenium.webdriver.support.ui import WebDriverWait\n"
-        "from selenium.webdriver.support import expected_conditions as EC\n"
-        "import time\n"
-        "import threading\n"
-        "import chromedriver_autoinstaller\n"
-        "chromedriver_autoinstaller.install()\n");
+    START_PYTHON
+    RUN_PYTHON(
+    from selenium.webdriver.chrome.options import Options;
+    from selenium import webdriver;
+    from selenium.webdriver.common.by import By;
+    from selenium.webdriver.support.ui import WebDriverWait;
+    from selenium.webdriver.support import expected_conditions as EC;
+    import time;
+    import threading;
+    import chromedriver_autoinstaller;
+    chromedriver_autoinstaller.install();
+    driver = webdriver.Chrome();
+    driver.get("https://www.google.com/");
+    );
 
-    PyRun_SimpleString("driver = webdriver.Chrome()\n");
-    PyRun_SimpleString("driver.get(\"https://www.google.com/\")\n");
+   
 
 }
